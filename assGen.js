4d624476fe5765bc2d08bceb06d463190f6ba560
Krittanon Wisedchart <Krittanon.w@gmail.com>
2016-05-19 16:17:42 +0700

    +

---
+++ assGen.js
@@ -0,0 +1,23 @@
+exports.add = function(op_l,op_r){
+    return `add ${op_l}, ${op_r}`;
+}
+
+exports.add = function(op_l,op_r){
+    return `sub ${op_l}, ${op_r}`;
+}
+
+exports.multi = function(op_l,op_r){
+    return `sub ${op_l}, ${op_r}`;
+}
+
+exports.divide = function(op_l,op_r){
+    return `sub ${op_l}, ${op_r}`;
+}
+
+exports.mod = function(op_l,op_r){
+    return `sub ${op_l}, ${op_r}`;
+}
+
+exports.add = function(op_l,op_r){
+    return `sub ${op_l}, ${op_r}`;
+}
